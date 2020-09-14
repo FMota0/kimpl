37de4c8bca5b57dbfc348a3552c02a3b3fab0441
Felipe Mota <felipe.santos@ccc.ufcg.edu.br>
2020-09-14 15:25:29 -0300

    Integrate tags page with api

--- client/src/components/Tags.js
+++ client/src/components/Tags.js
@@ -1,20 +1,34 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-
-const tags = [
-  'greedy',
-  'dynamic programming',
-  'segtree',
-  'aho corasick',
-  'flow',
-  'convex hull',
-  'fenwick tree',
-  'fft',
-  'dfs',
-  'bfs',
-];
+import axios from "axios";
 
 const Tags = () => {
+  const [ tags, setTags ] = useState([]);
+  const [ isLoading, setIsLoading ] = useState(true);
+
+  useEffect(() => {
+    (async () => {
+      const { data: fetchedTags } = await axios.get('tags');
+      setTags(fetchedTags);
+      setIsLoading(false);
+    })();
+  }, [])
+
+  if (isLoading) {
+    return (
+      <div className="flex flex-wrap">
+        {Array(20).fill(0).map((_, i) => {
+          return (
+            <div key={i} className="pa2 w-20 f3">
+              <div className="ba br2 card-load loading tc">
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    )
+  }
+
   return (
     <div className="flex flex-wrap">
       {tags.map(tag => {
@@ -22,7 +36,7 @@ const Tags = () => {
           <div key={tag} className="pa2 w-20 f3">
             <Link to={`/tags/${tag}`} style={{textDecoration: 'none'}}> 
               <div className="ba br2 card tc">
-                  <p>{tag}</p>
+                <p> {tag} </p>
               </div>
             </Link>
           </div>
